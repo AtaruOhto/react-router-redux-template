d93df02b195468aa382771eda31872322bc7ad5b
Oto Ataru <i.oto.ataru@globis.co.jp>
2018-01-23 18:20:42 +0900

    trivial commit

---
+++ front/stores/aaa.ts
@@ -0,0 +1,11 @@
+export class Hoge {
+
+    constructor() {
+
+    }
+
+    fuga() {
+        return 1
+    }
+
+}
---
+++ front/stores/hoge.spec.ts
@@ -0,0 +1,11 @@
+import {Hoge} from 'stores/aaa'
+import {} from 'jasmine';
+
+describe('ssss', () => {
+    it('should...', () => {
+        const hoge = new Hoge();
+        expect(hoge.fuga() + 1).toBe(2)
+    })
+})
+
+
